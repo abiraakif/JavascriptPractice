dab60ee56be0837c3cee41956f9695f6014ef248
Abeera Akif <abira.akif@gmail.com>
2022-07-17 18:04:03 +0500

    Initial commit

---
+++ code1.js
@@ -0,0 +1,8 @@
+let a=6;
+let b=9;
+
+x=a+b;
+y=a*b;
+console.log(x);
+console.log(y);
+console.log((x)+(y));
---
+++ functions.js
@@ -0,0 +1,16 @@
+function add(x,y) {
+    return x+y;
+}
+
+let result=add(6,9);
+console.log(result);
+
+let a=add(10,11);
+console.log(a);
+
+
+function add(x,y,z) {
+return x+y+z;
+let b=add(7,5,6);
+console.log(b);
+}
